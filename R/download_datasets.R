97b882c71b06cac3172f630e0311deaaff128e90
lautarosanfi <lautarosanfilippo1@gmail.com>
2024-10-25 10:43:19 -0300

    Code of conduct and fixing read and download functions

--- R/download_datasets.R
+++ R/download_datasets.R
@@ -1,13 +1,13 @@
 #' Downloading datasets
 #'
-#'The function download_datasets() creates the 'data' folder if it does not already exist and downloads all files related to meteorology.
+#'The function download_datasets() creates the 'data-raw' folder if it does not already exist and downloads all files related to meteorology.
 #'
 #'WARNING: IT WILL OVERWRITE EXISTING DATASETS!
 #'
 #' @export
 download_datasets <- function(){
   library(readr)
-  dir.create("data_raw", showWarnings = FALSE)
+  dir.create("data-raw", showWarnings = FALSE)
   urls <- c(
     "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/metadatos_completos.csv",
     "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0472.csv",
@@ -29,7 +29,5 @@ download_datasets <- function(){
     download.file(url = urls[i], destfile = file.path("data-raw", nombres_archivos[i]))
   }
 
-  metereological_data <- rbind(metadata.csv,station_NH0472.csv, station_NH0910.csv, station_NH0046.csv, station_NH0098.csv, station_NH0437.csv)
-  usethis::use_data("metereological_data", overwrite = TRUE)
 
 }
--- R/read_datasets.R
+++ R/read_datasets.R
@@ -1,15 +1,15 @@
 #' Read datasets
 #'
-#'The function read_datasets() reads all files related to meteorology from 'data_raw' folder and loads them into the global environment.
+#'The function read_datasets() reads all files related to meteorology from 'data-raw' folder and loads them into the global environment.
 #'
 #' @export
 read_datasets <- function(){
-  metadata <- read_csv("data_raw/metadata.csv")
-  station_NH0472 <- read_csv("data_raw/station_NH0472.csv")
-  station_NH0910 <- read_csv("data_raw/station_NH0910.csv")
-  station_NH0046 <- read_csv("data_raw/station_NH0046.csv")
-  station_NH0098 <- read_csv("data_raw/station_NH0098.csv")
-  station_NH0437 <- read_csv("data_raw/station_NH0437.csv")
+  metadata <- read_csv("data-raw/metadata.csv")
+  station_NH0472 <- read_csv("data-raw/station_NH0472.csv")
+  station_NH0910 <- read_csv("data-raw/station_NH0910.csv")
+  station_NH0046 <- read_csv("data-raw/station_NH0046.csv")
+  station_NH0098 <- read_csv("data-raw/station_NH0098.csv")
+  station_NH0437 <- read_csv("data-raw/station_NH0437.csv")
 
   assign("metadata", metadata, envir = .GlobalEnv)
   assign("station_NH0472", station_NH0472, envir = .GlobalEnv)
@@ -17,4 +17,7 @@ read_datasets <- function(){
   assign("station_NH0046", station_NH0046, envir = .GlobalEnv)
   assign("station_NH0098", station_NH0098, envir = .GlobalEnv)
   assign("station_NH0437", station_NH0437, envir = .GlobalEnv)
+
+  metereological_data <- rbind(station_NH0472, station_NH0910, station_NH0046, station_NH0098, station_NH0437)
+  usethis::use_data(metereological_data, overwrite = TRUE)
 }
