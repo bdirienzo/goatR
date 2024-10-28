2d6d283e2c6ee3a84a4ab7ae353172cf6cae1d9e
Bernardo Di Rienzo <bdirienzo@mail.austral.edu.ar>
2024-10-28 11:53:54 -0300

    testing

--- tests/testthat/test-download_datasets.R
+++ tests/testthat/test-download_datasets.R
@@ -25,7 +25,7 @@ test_that("dataset download works", {
     download_datasets("metadatos_completos")
     expect_true(file.exists("datasets-raw/metadatos_completos.csv"))
 
-    # Test 7: Verificar que todos los archivos se hayan descargado
+    # Test 7: Verificar que todos los archivos se hayan descargado cuando id_station es NULL
     download_datasets()
     expect_true(file.exists("datasets-raw/NH0472.csv"))
     expect_true(file.exists("datasets-raw/NH0910.csv"))
@@ -39,38 +39,41 @@ test_that("dataset download works", {
 test_that("download_datasets handles errors correctly", {
   withr::with_tempdir({
     # Test 1: Mock de download.file para que genere un error
-      with_mocked_bindings({
-        download.file = function(url, destfile, ...) {
-          stop("Simulated download error")
-        }
-        expect_error(
-          download_datasets("NH0472"),
-          "Download failed. Please check the station ID and your internet connection. Error: Simulated download error"
-        )
-      })
+    withr::local_mock(
+      download.file = function(url, destfile, ...) {
+        stop("Simulated download error")
+      }
+    )
+    expect_error(
+      download_datasets("NH0472"),
+      "Download failed. Please check the station ID and your internet connection. Error: Simulated download error"
+    )
 
-  # Test 2: Mock de download.file para que genere una advertencia
-  with_mocked_bindings({
-    download.file = function(url, destfile, ...) {
-      warning("Simulated download warning")
-      # Asegurarse de que el archivo se descarga para no provocar un error adicional
-      file.create(destfile)
-    }
+    # Test 2: Mock de download.file para que genere una advertencia
+    withr::local_mock(
+      download.file = function(url, destfile, ...) {
+        warning("Simulated download warning")
+        # Crear el archivo para no provocar otro error
+        file.create(destfile)
+      }
+    )
     expect_warning(
       download_datasets("NH0472"),
       "Download may have issues: Simulated download warning"
     )
-  })
-  # Test 3: Mock de download.file para que cree un archivo vacío
-  with_mocked_bindings({
-    download.file = function(url, destfile, ...) {
-      file.create(destfile)
-    }
+
+    # Test 3: Mock de download.file para que cree un archivo vacío
+    withr::local_mock(
+      download.file = function(url, destfile, ...) {
+        # Crear un archivo vacío
+        file.create(destfile)
+      }
+    )
     expect_error(
       download_datasets("NH0472"),
       "Download failed. The file is either missing or empty."
     )
-  })
+
     # Test 4: ID de estación inválido
     expect_error(
       download_datasets("NH00000"),
