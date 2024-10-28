715db86ac7eea732cb67f4bf7d8b20448671b9ba
Bernardo Di Rienzo <bdirienzo@mail.austral.edu.ar>
2024-10-28 11:49:11 -0300

    testeo

--- tests/testthat/test-download_datasets.R
+++ tests/testthat/test-download_datasets.R
@@ -24,19 +24,60 @@ test_that("dataset download works", {
     # Test 6: Descargar archivo "metadatos_completos" al directorio por defecto en el directorio temporal
     download_datasets("metadatos_completos")
     expect_true(file.exists("datasets-raw/metadatos_completos.csv"))
+
+    # Test 7: Verificar que todos los archivos se hayan descargado
+    download_datasets()
+    expect_true(file.exists("datasets-raw/NH0472.csv"))
+    expect_true(file.exists("datasets-raw/NH0910.csv"))
+    expect_true(file.exists("datasets-raw/NH0046.csv"))
+    expect_true(file.exists("datasets-raw/NH0098.csv"))
+    expect_true(file.exists("datasets-raw/NH0437.csv"))
+    expect_true(file.exists("datasets-raw/metadatos_completos.csv"))
   })
 })
 
 test_that("download_datasets handles errors correctly", {
-  # Ejecutar las pruebas de manejo de errores dentro de un directorio temporal
   withr::with_tempdir({
-    # Test 1: ID de estación inválido
+    # Test 1: Mock de download.file para que genere un error
+      with_mocked_bindings({
+        download.file = function(url, destfile, ...) {
+          stop("Simulated download error")
+        }
+        expect_error(
+          download_datasets("NH0472"),
+          "Download failed. Please check the station ID and your internet connection. Error: Simulated download error"
+        )
+      })
+
+  # Test 2: Mock de download.file para que genere una advertencia
+  with_mocked_bindings({
+    download.file = function(url, destfile, ...) {
+      warning("Simulated download warning")
+      # Asegurarse de que el archivo se descarga para no provocar un error adicional
+      file.create(destfile)
+    }
+    expect_warning(
+      download_datasets("NH0472"),
+      "Download may have issues: Simulated download warning"
+    )
+  })
+  # Test 3: Mock de download.file para que cree un archivo vacío
+  with_mocked_bindings({
+    download.file = function(url, destfile, ...) {
+      file.create(destfile)
+    }
+    expect_error(
+      download_datasets("NH0472"),
+      "Download failed. The file is either missing or empty."
+    )
+  })
+    # Test 4: ID de estación inválido
     expect_error(
       download_datasets("NH00000"),
       "Invalid station ID. Valid options are: NH0472, NH0910, NH0046, NH0098, NH0437, metadatos_completos"
     )
 
-    # Test 2: Directorio no existente
+    # Test 5: Directorio no existente
     expect_error(
       download_datasets("NH0437", "./testing/NH0437.csv"),
       "The specified directory does not exist."
