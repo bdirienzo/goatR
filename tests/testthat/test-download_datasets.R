fb8a9c6ba26d9f5802de00af59f8e11b52eeac5e
lautarosanfi <lautarosanfilippo1@gmail.com>
2024-11-21 21:41:20 -0300

    test

--- tests/testthat/test-download_datasets.R
+++ tests/testthat/test-download_datasets.R
@@ -1,61 +1,57 @@
-library(testthat)
-library(withr)
-library(mockery)
-
-test_that("download_datasets works when id_station is NULL", {
-  with_tempdir({
+testthat::test_that("download_datasets works when id_station is NULL", {
+  withr::with_tempdir({
     download_datasets()
-    expect_true(file.exists("datasets-raw/NH0472.csv"))
-    expect_true(file.exists("datasets-raw/NH0910.csv"))
-    expect_true(file.exists("datasets-raw/NH0046.csv"))
-    expect_true(file.exists("datasets-raw/NH0098.csv"))
-    expect_true(file.exists("datasets-raw/NH0437.csv"))
-    expect_true(file.exists("datasets-raw/metadatos_completos.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0472.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0910.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0046.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0098.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0437.csv"))
+    testthat::expect_true(file.exists("datasets-raw/metadatos_completos.csv"))
   })
 })
 
-test_that("download_datasets works with a valid id_station", {
-  with_tempdir({
+testthat::test_that("download_datasets works with a valid id_station", {
+  withr::with_tempdir({
     download_datasets("NH0472")
-    expect_true(file.exists("datasets-raw/NH0472.csv"))
+    testthat::expect_true(file.exists("datasets-raw/NH0472.csv"))
   })
 })
 
-test_that("download_datasets handles invalid id_station", {
-  with_tempdir({
-    expect_error(
+testthat::test_that("download_datasets handles invalid id_station", {
+  withr::with_tempdir({
+    testthat::expect_error(
       download_datasets("INVALID_ID"),
       "Invalid station ID. Valid options are:"
     )
   })
 })
 
-test_that("download_datasets saves file to specified path", {
-  with_tempdir({
+testthat::test_that("download_datasets saves file to specified path", {
+  withr::with_tempdir({
     dir.create("custom_folder")
     custom_path <- file.path("custom_folder", "NH0472.csv")
     download_datasets("NH0472", path = custom_path)
-    expect_true(file.exists(custom_path))
+    testthat::expect_true(file.exists(custom_path))
   })
 })
 
-test_that("download_datasets handles non-existent directory", {
-  with_tempdir({
+testthat::test_that("download_datasets handles non-existent directory", {
+  withr::with_tempdir({
     custom_path <- file.path("nonexistent_directory", "NH0472.csv")
-    expect_error(
+    testthat::expect_error(
       download_datasets("NH0472", path = custom_path),
       "The specified directory does not exist."
     )
   })
 })
 
-test_that("download_datasets handles download errors", {
-  with_tempdir({
+testthat::test_that("download_datasets handles download errors", {
+  withr::with_tempdir({
     mock_download_file_error <- function(url, destfile, ...) {
       stop("Simulated download error")
     }
-    stub(download_datasets, 'download.file', mock_download_file_error)
-    expect_error(
+    mockery::stub(download_datasets, 'download.file', mock_download_file_error)
+    testthat::expect_error(
       download_datasets("NH0472"),
       "Download failed. Please check the station ID and your internet connection. Error: Simulated download error"
     )
--- tests/testthat/test-read_datasets.R
+++ tests/testthat/test-read_datasets.R
@@ -1,27 +1,25 @@
-library(testthat)
-
-test_that("read_datasets reads data correctly with id_station", {
+testthat::test_that("read_datasets reads data correctly with id_station", {
   data_read <- read_datasets(id_station = "NH0472")
-  data_original <- read.csv(file.path("datasets-raw", "NH0472.csv"))
-  expect_equal(data_read, data_original)
+  data_original <- utils::read.csv(base::file.path("datasets-raw", "NH0472.csv"))
+  testthat::expect_equal(data_read, data_original)
 })
 
-test_that("read_datasets throws error when file does not exist", {
-  expect_error(
+testthat::test_that("read_datasets throws error when file does not exist", {
+  testthat::expect_error(
     read_datasets(id_station = "INVALID_ID"),
     "The file or path doesn't exist"
   )
 })
 
-test_that("read_datasets reads data correctly with path", {
-  path <- file.path("datasets-raw", "NH0472.csv")
+testthat::test_that("read_datasets reads data correctly with path", {
+  path <- base::file.path("datasets-raw", "NH0472.csv")
   data_read <- read_datasets(path = path)
-  data_original <- read.csv(path)
-  expect_equal(data_read, data_original)
+  data_original <- utils::read.csv(path)
+  testthat::expect_equal(data_read, data_original)
 })
 
-test_that("read_datasets throws error when neither id_station nor path are provided", {
-  expect_error(
+testthat::test_that("read_datasets throws error when neither id_station nor path are provided", {
+  testthat::expect_error(
     read_datasets(),
     "Either 'id_station' or 'path' must be provided."
   )
